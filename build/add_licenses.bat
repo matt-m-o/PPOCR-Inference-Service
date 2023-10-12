738f85d2add1eb378afb4a0f00e95116612c0fa8
MatheusM8 <matheusmoura8@hotmail.com>
2023-10-11 22:22:46 -0300

    Added license

---
+++ build/add_licenses.bat
@@ -0,0 +1,7 @@
+xcopy ..\LICENSE .\Release\LICENSE.txt /e
+
+xcopy ..\includes\cpp-httplib-0.14.0\LICENSE .\Release\third-party-licenses\cpp-httplib.txt /e
+xcopy ..\includes\cpp-base64-2.rc.08\LICENSE .\Release\third-party-licenses\cpp-base64.txt /e
+xcopy ..\includes\nlohmann\LICENSE.MIT .\Release\third-party-licenses\nlohmann_json.txt /e
+xcopy ..\sdk\fastdeploy-win-x64-1.0.7\LICENSE .\Release\third-party-licenses\fastdeploy.txt /e
+xcopy ..\sdk\fastdeploy-win-x64-1.0.7\ThirdPartyNotices.txt .\Release\third-party-licenses\fastdeploy.third_party_notices.txt /e
